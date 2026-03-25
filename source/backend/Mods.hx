ea560669f88ec7c84cabb0ca8ed9e3251867a7eb
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-03-24 20:24:08 -0700

    switch to old mod path

--- source/backend/Mods.hx
+++ source/backend/Mods.hx
@@ -38,7 +38,7 @@ class Mods
 
 	inline public static function getModPath(key:String = ''):String {
 		#if android
-		return lime.system.System.applicationStorageDirectory + "files/mods/" + key;
+		return "/storage/emulated/0/Android/data/com.shadowmario.psychengine/files/mods/" + key;
 		#else
 		return Paths.mods(key);
 		#end
