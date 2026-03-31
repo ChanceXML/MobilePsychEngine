c611a1b50d06c568b33558ba325a16477772c526
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-03-31 13:23:21 -0700

    Update ControlsSubState.hx

--- source/options/ControlsSubState.hx
+++ source/options/ControlsSubState.hx
@@ -33,6 +33,7 @@ class ControlsSubState extends MusicBeatSubstate
 		[true, 'Accept', 'accept', 'Accept'],
 		[true, 'Back', 'back', 'Back'],
 		[true, 'Pause', 'pause', 'Pause'],
+		[true, 'Control', 'control', 'Control'],
 		[false],
 		[false, 'VOLUME'],
 		[false, 'Mute', 'volume_mute', 'Volume Mute'],
