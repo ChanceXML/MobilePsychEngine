9e5dabdfe4cfd48d6a9195a93ee4dd5b3567f857
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-03-31 15:34:31 -0700

    Update ButtonHelper.hx

--- source/android/utils/ButtonHelper.hx
+++ source/android/utils/ButtonHelper.hx
@@ -2,40 +2,19 @@ package android.utils;
 
 #if mobile
 import android.controls.VirtualPad;
-import flixel.group.FlxGroup;
 import flixel.FlxBasic;
-import backend.Controls;
+import flixel.group.FlxGroup;
 #end
 
 class ButtonHelper
 {
 	#if mobile
+
 	public static function create(parent:FlxBasic, dpad:VirtualPad.DPadMode, action:VirtualPad.ActionMode):VirtualPad
 	{
 		var vpad = new VirtualPad(dpad, action);
-
-		var group:FlxGroup = cast parent;
-		if (group != null)
-			group.add(vpad);
-
-		Controls.virtualPad = vpad;
+		cast(parent, FlxGroup).add(vpad);
 		return vpad;
-	}
-
-	public static function setupMenu(vpad:VirtualPad):Void
-	{
-		if (vpad == null) return;
-
-		vpad.bindDPad('ui_up', 'ui_down', 'ui_left', 'ui_right');
-		vpad.bindActionGroup('accept', 'back');
-	}
-
-	public static function setupGameplay(vpad:VirtualPad):Void
-	{
-		if (vpad == null) return;
-
-		vpad.bindDPad('note_up', 'note_down', 'note_left', 'note_right');
-		vpad.bindActionGroup('note_left', 'note_down', 'note_up', 'note_right');
 	}
 
 	public static function bind(vpad:VirtualPad, dpad:Array<String>, actions:Array<String>):Void
@@ -43,7 +22,9 @@ class ButtonHelper
 		if (vpad == null) return;
 
 		if (dpad != null && dpad.length >= 4)
+		{
 			vpad.bindDPad(dpad[0], dpad[1], dpad[2], dpad[3]);
+		}
 
 		if (actions != null)
 		{
@@ -54,9 +35,9 @@ class ButtonHelper
 				case 3: vpad.bindActionGroup(actions[0], actions[1], actions[2]);
 				case 4: vpad.bindActionGroup(actions[0], actions[1], actions[2], actions[3]);
 				case 5: vpad.bindActionGroup(actions[0], actions[1], actions[2], actions[3], actions[4]);
-				default:
 			}
 		}
 	}
+
 	#end
 }
