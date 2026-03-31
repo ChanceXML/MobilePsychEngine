c142d1af67c0aa8db332a7550411f43afc49332c
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-03-31 15:44:11 -0700

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -21,6 +21,9 @@ enum MainMenuColumn {
 
 class MainMenuState extends MusicBeatState
 {
+	#if mobile
+    var virtualPad:VirtualPad;
+    #end
 	public static var psychEngineVersion:String = '1.0.4'; // This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 	public static var curColumn:MainMenuColumn = CENTER;
