11c8a12d0e19494ab213fbaa36e6843e9cc4f50a
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-03-31 10:54:19 -0700

    Update Mouse.hx

--- source/android/utils/Mouse.hx
+++ source/android/utils/Mouse.hx
@@ -5,45 +5,42 @@ import flixel.FlxSprite;
 import flixel.FlxCamera;
 import flixel.input.touch.FlxTouch;
 import flixel.math.FlxMath;
+import openfl.utils.Assets;
 
 class Mouse extends FlxSprite {
-    public var mouse:Bool = false;
+    public var mouseEnabled:Bool = false;
     
     var lastTouchX:Float = 0;
     var lastTouchY:Float = 0;
     var isDragging:Bool = false;
     
-    var prevX:Float = 0;
-    var prevY:Float = 0;
-    
-    public var camMouse:FlxCamera;
+    public var mouseCamera:FlxCamera;
 
     public function new() {
         super();
         
-        camMouse = new FlxCamera(0, 0, FlxG.width, FlxG.height);
-        camMouse.bgColor = 0x00000000;
-        camMouse.alpha = 1;
-        FlxG.cameras.add(camMouse, false);
+        mouseCamera = new FlxCamera(0, 0, FlxG.width, FlxG.height);
+        mouseCamera.bgColor = 0x00000000;
+        FlxG.cameras.add(mouseCamera, false);
         
-        this.cameras = [camMouse];
+        this.cameras = [mouseCamera];
         
         loadCustomGraphic("cursor-default");
         antialiasing = true;
         scrollFactor.set(0, 0);
         
-        this.x = (FlxG.width - width) / 2;
-        this.y = (FlxG.height - height) / 2;
+        this.x = FlxG.width / 2;
+        this.y = FlxG.height / 2;
     }
 
     override public function update(elapsed:Float):Void {
         #if mobile
         if (ClientPrefs.data != null) {
-            mouse = ClientPrefs.data.mouse;
+            mouseEnabled = ClientPrefs.data.mouse;
         }
         #end
 
-        if (!mouse) {
+        if (!mouseEnabled) {
             visible = false;
             return;
         }
@@ -51,17 +48,12 @@ class Mouse extends FlxSprite {
         visible = true;
         FlxG.mouse.visible = false;
 
-        if (FlxG.cameras.list != null && FlxG.cameras.list.length > 0) {
-            if (FlxG.cameras.list[FlxG.cameras.list.length - 1] != camMouse) {
-                FlxG.cameras.remove(camMouse, false);
-                FlxG.cameras.add(camMouse, false);
-            }
+        if (FlxG.cameras.list.indexOf(mouseCamera) != FlxG.cameras.list.length - 1) {
+            FlxG.cameras.remove(mouseCamera, false);
+            FlxG.cameras.add(mouseCamera, false);
         }
 
         var touch:FlxTouch = FlxG.touches.getFirst();
-
-        prevX = this.x;
-        prevY = this.y;
 
         @:privateAccess {
             if (touch != null) {
@@ -71,12 +63,9 @@ class Mouse extends FlxSprite {
                     isDragging = true;
                     FlxG.mouse._leftButton.press();
                 } else if (touch.pressed && isDragging) {
-                    var deltaX:Float = touch.screenX - lastTouchX;
-                    var deltaY:Float = touch.screenY - lastTouchY;
-
-                    this.x += deltaX;
-                    this.y += deltaY;
-
+                    this.x += (touch.screenX - lastTouchX);
+                    this.y += (touch.screenY - lastTouchY);
+                    
                     lastTouchX = touch.screenX;
                     lastTouchY = touch.screenY;
                 } else if (touch.justReleased) {
@@ -85,27 +74,18 @@ class Mouse extends FlxSprite {
                 }
             } else {
                 isDragging = false;
-                if (FlxG.mouse._leftButton.pressed) {
-                    FlxG.mouse._leftButton.release();
-                }
+                if (FlxG.mouse._leftButton.pressed) FlxG.mouse._leftButton.release();
             }
 
-            this.x = FlxMath.bound(this.x, 0, FlxG.width - width);
-            this.y = FlxMath.bound(this.y, 0, FlxG.height - height);
+            this.x = FlxMath.bound(this.x, 0, FlxG.width);
+            this.y = FlxMath.bound(this.y, 0, FlxG.height);
 
             FlxG.mouse.screenX = Std.int(this.x);
             FlxG.mouse.screenY = Std.int(this.y);
             
             if (FlxG.camera != null) {
-                FlxG.mouse.x = (this.x / FlxG.camera.zoom) + FlxG.camera.scroll.x;
-                FlxG.mouse.y = (this.y / FlxG.camera.zoom) + FlxG.camera.scroll.y;
-            } else {
-                FlxG.mouse.x = Std.int(this.x);
-                FlxG.mouse.y = Std.int(this.y);
-            }
-
-            if (this.x != prevX || this.y != prevY) {
-                FlxG.mouse.justMoved = true;
+                FlxG.mouse.x = Std.int((this.x / FlxG.camera.zoom) + FlxG.camera.scroll.x);
+                FlxG.mouse.y = Std.int((this.y / FlxG.camera.zoom) + FlxG.camera.scroll.y);
             }
         }
 
@@ -113,27 +93,23 @@ class Mouse extends FlxSprite {
     }
 
     public function setHoverState(isHovering:Bool):Void {
-        if (!mouse) return;
+        if (!mouseEnabled) return;
         loadCustomGraphic(isHovering ? "cursor-pointer" : "cursor-default");
     }
 
     private function loadCustomGraphic(name:String):Void {
-        var imagePath:String = "mouse/" + name;
-        try {
-            var fallbackPath:String = "assets/images/" + imagePath + ".png";
-            if (openfl.utils.Assets.exists(fallbackPath)) {
-                loadGraphic(fallbackPath);
-            }
-        } catch(e:Dynamic) {
-            visible = false;
-            FlxG.mouse.visible = true;
+        var path:String = "assets/images/mouse/" + name + ".png";
+        if (Assets.exists(path)) {
+            loadGraphic(path);
+        } else {
+            makeGraphic(15, 15, 0xFFFFFFFF);
         }
     }
     
     override public function destroy():Void {
-        if (camMouse != null) {
-            FlxG.cameras.remove(camMouse);
-            camMouse.destroy();
+        if (mouseCamera != null) {
+            FlxG.cameras.remove(mouseCamera);
+            mouseCamera = null;
         }
         super.destroy();
     }
