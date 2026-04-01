53059be1cf2e12529760a0d77a6b901a0aad7d0e
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-04-01 09:41:09 -0700

    Update ButtonHelper.hx

--- source/android/utils/ButtonHelper.hx
+++ source/android/utils/ButtonHelper.hx
@@ -2,36 +2,35 @@ package android.utils;
 
 #if mobile
 import android.controls.VirtualPad;
-import flixel.group.FlxGroup;
 #end
 
 class ButtonHelper
 {
 	#if mobile
 
-	public static function create(parent:FlxGroup, ?dpad:Dynamic = null, ?action:Dynamic = null):VirtualPad
+	public static function create(parent:Dynamic, dpad:Dynamic, action:Dynamic):VirtualPad
 	{
-		var pad = new VirtualPad(dpad, action);
-		parent.add(pad);
-		return pad;
+		var vpad = new VirtualPad(dpad, action);
+		if (parent != null) parent.add(vpad);
+		return vpad;
 	}
 
-	public static function bind(pad:VirtualPad, dpad:Array<String>, actions:Array<String>):Void
+	public static function bind(vpad:VirtualPad, dpad:Array<String>, actions:Array<String>):Void
 	{
-		if (pad == null) return;
+		if (vpad == null) return;
 
 		if (dpad != null && dpad.length >= 4)
-			pad.bindDPad(dpad[0], dpad[1], dpad[2], dpad[3]);
+			vpad.bindDPad(dpad[0], dpad[1], dpad[2], dpad[3]);
 
 		if (actions != null)
 		{
 			switch (actions.length)
 			{
-				case 1: pad.bindActionGroup(actions[0]);
-				case 2: pad.bindActionGroup(actions[0], actions[1]);
-				case 3: pad.bindActionGroup(actions[0], actions[1], actions[2]);
-				case 4: pad.bindActionGroup(actions[0], actions[1], actions[2], actions[3]);
-				case 5: pad.bindActionGroup(actions[0], actions[1], actions[2], actions[3], actions[4]);
+				case 1: vpad.bindActionGroup(actions[0]);
+				case 2: vpad.bindActionGroup(actions[0], actions[1]);
+				case 3: vpad.bindActionGroup(actions[0], actions[1], actions[2]);
+				case 4: vpad.bindActionGroup(actions[0], actions[1], actions[2], actions[3]);
+				case 5: vpad.bindActionGroup(actions[0], actions[1], actions[2], actions[3], actions[4]);
 				default:
 			}
 		}
