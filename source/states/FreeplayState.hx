dc2534e130891cdbe799bca4ce41d7bcd50f401e
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-04-03 11:04:43 -0700

    move something to the top

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -216,11 +216,12 @@ class FreeplayState extends MusicBeatState
 	}
 
 	override function closeSubState()
-	{
-		changeSelection(0, false);
-		persistentUpdate = true;
-		super.closeSubState();
-	}
+{
+    super.closeSubState();
+
+    changeSelection(0, false);
+    persistentUpdate = true;
+}
 
 	public function addSong(songName:String, weekNum:Int, songCharacter:String, color:Int)
 	{
