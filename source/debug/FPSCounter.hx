8fd086a706578525de63ae6060f9282a23329808
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-04-03 12:00:28 -0700

    Update FPSCounter.hx

--- source/debug/FPSCounter.hx
+++ source/debug/FPSCounter.hx
@@ -5,28 +5,21 @@ import openfl.text.TextField;
 import openfl.text.TextFormat;
 import openfl.system.System;
 
-/**
-	The FPS class provides an easy-to-use monitor to display
-	the current frame rate of an OpenFL project
-**/
 class FPSCounter extends TextField
 {
-	/**
-		The current frame rate, expressed using frames-per-second
-	**/
 	public var currentFPS(default, null):Int;
-
-	/**
-		The current memory usage (WARNING: this is NOT your total program memory usage, rather it shows the garbage collector memory)
-	**/
 	public var memoryMegas(get, never):Float;
 
-	@:noCompletion private var times:Array<Float>;
+	private var times:Array<Float>;
+	public var offsetX:Float;
+	public var offsetY:Float;
 
 	public function new(x:Float = 10, y:Float = 10, color:Int = 0x000000)
 	{
 		super();
 
+		this.offsetX = x;
+		this.offsetY = y;
 		this.x = x;
 		this.y = y;
 
@@ -43,13 +36,22 @@ class FPSCounter extends TextField
 
 	var deltaTimeout:Float = 0.0;
 
-	// Event Handlers
 	private override function __enterFrame(deltaTime:Float):Void
 	{
+		if (FlxG.game != null) {
+			if (ClientPrefs.data != null) {
+				this.scaleX = ClientPrefs.data.fpsSize;
+				this.scaleY = ClientPrefs.data.fpsSize;
+			}
+
+			this.x = FlxG.game.x + (offsetX * this.scaleX);
+			this.y = FlxG.game.y + (offsetY * this.scaleY);
+		}
+
 		final now:Float = haxe.Timer.stamp() * 1000;
 		times.push(now);
 		while (times[0] < now - 1000) times.shift();
-		// prevents the overlay from updating every frame, why would you need to anyways @crowplexus
+
 		if (deltaTimeout < 50) {
 			deltaTimeout += deltaTime;
 			return;
@@ -60,7 +62,7 @@ class FPSCounter extends TextField
 		deltaTimeout = 0.0;
 	}
 
-	public dynamic function updateText():Void { // so people can override it in hscript
+	public dynamic function updateText():Void {
 		text = 'FPS: ${currentFPS}'
 		+ '\nMemory: ${flixel.util.FlxStringUtil.formatBytes(memoryMegas)}';
 
