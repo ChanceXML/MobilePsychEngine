dd75e5cee0767dbbd4d976870f2f5a38fe567140
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-04-19 11:59:08 -0700

    Update IOSNative.hx

--- source/backend/IOSNative.hx
+++ source/backend/IOSNative.hx
@@ -1,19 +1,15 @@
 package backend;
 
 #if ios
-
 @:cppFileCode('extern "C" void ios_show_alert(const char* title, const char* message);')
 extern class IOSNative
 {
     @:native("ios_show_alert")
     public static function showAlert(title:String, message:String):Void;
 }
-
 #else
-
 class IOSNative
 {
     public static function showAlert(title:String, message:String):Void {}
 }
-
 #end
