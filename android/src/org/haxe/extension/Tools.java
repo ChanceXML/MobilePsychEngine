837961bd6e6397ae9c3d7f7b69dd7ee542d811f7
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-04-19 09:39:08 -0700

    Update Tools.java

--- android/src/org/haxe/extension/Tools.java
+++ android/src/org/haxe/extension/Tools.java
@@ -1,25 +1,25 @@
 package org.haxe.extension;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Looper;
+import android.os.Handler;
 
-public class Tools extends Extension
-{
-    public static void showMessage(final String title, final String message)
-    {
-        if (mainActivity == null) return;
-
-        mainActivity.runOnUiThread(new Runnable()
-        {
+public class Tools extends Extension {
+    public static void showMessage(final String title, final String message) {
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
             @Override
-            public void run()
-            {
-                new AlertDialog.Builder(mainActivity)
-                    .setTitle(title)
-                    .setMessage(message)
-                    .setCancelable(false)
-                    .setPositiveButton("OK", null)
-                    .show();
+            public void run() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Extension.mainActivity);
+                builder.setTitle(title);
+                builder.setMessage(message);
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.setCancelable(false);
+                builder.create().show();
             }
         });
     }
