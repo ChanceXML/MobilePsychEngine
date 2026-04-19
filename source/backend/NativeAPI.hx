38f01032168e7ef3dd06d0a33afa9d85fe805607
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-04-19 13:24:52 -0700

    button

--- source/backend/NativeAPI.hx
+++ source/backend/NativeAPI.hx
@@ -135,17 +135,18 @@ class NativeAPI {
 	public static function hasVersion(vers:String)
 		return lime.system.System.platformLabel.toLowerCase().indexOf(vers.toLowerCase()) != -1;
 
-	public static function showMessageBox(caption:String, message:String, icon:MessageBoxIcon = MSG_WARNING)
-	{
-		#if android
-		extension.androidtools.Tools.showAlertDialog(caption, message);
-		#elseif (windows && !macro)
-		var iconInt:Int = cast(icon, Int);
-		untyped __cpp__('MessageBoxA(GetActiveWindow(), {0}.c_str(), {1}.c_str(), {2})', message, caption, iconInt);
-		#else
-		lime.app.Application.current.window.alert(message, caption);
-		#end
-	}
+	public static function showMessageBox(caption:String, message:String, buttonLabel:String = "OK", icon:MessageBoxIcon = MSG_WARNING)
+{
+	#if android
+	extension.androidtools.Tools.showAlertDialog(caption, message, buttonLabel, null, null, null);
+	#elseif (windows && !macro)
+	var iconInt:Int = cast(icon, Int);
+	untyped __cpp__('MessageBoxA(GetActiveWindow(), {0}.c_str(), {1}.c_str(), {2})', message, caption, iconInt);
+	#else
+	lime.app.Application.current.window.alert(message, caption);
+	#end
+}
+	
 
 	public static function showToast(message:String)
 	{
