e78356f3884ddadb4d1f06add5dfba20e6817fee
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-04-18 19:46:44 -0700

    fixed error

--- source/backend/IOSNative.hx
+++ source/backend/IOSNative.hx
@@ -1,14 +1,18 @@
 package backend;
 
 #if ios
+
 @:native("ios_show_alert")
 extern class IOSNative
 {
     public static function ios_show_alert(title:String, message:String):Void;
 }
+
 #else
+
 class IOSNative
 {
     public static function ios_show_alert(title:String, message:String):Void {}
 }
+
 #end
