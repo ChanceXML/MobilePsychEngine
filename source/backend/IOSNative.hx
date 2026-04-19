459894da2fca7fcf24f3ed7e5560b8b467a37557
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-04-18 18:49:06 -0700

    Update IOSNative.hx

--- source/backend/IOSNative.hx
+++ source/backend/IOSNative.hx
@@ -1,18 +1,14 @@
 package backend;
 
 #if ios
-
 @:native("ios_show_alert")
 extern class IOSNative
 {
     public static function ios_show_alert(title:String, message:String):Void;
 }
-
 #else
-
 class IOSNative
 {
     public static function ios_show_alert(title:String, message:String):Void {}
 }
-
 #end
