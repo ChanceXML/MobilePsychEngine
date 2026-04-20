98b35453480f1e9ff588a7edb9dd2ec8dcd80f5f
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-04-19 19:13:54 -0700

    Update IOSNative.hx

--- source/backend/IOSNative.hx
+++ source/backend/IOSNative.hx
@@ -1,6 +1,11 @@
 package backend;
 
 #if ios
+// This tells the compiler to find and compile the objective-C++ file
+// The path is relative to the project's export/ios/build folder
+@:buildXml('<target id="haxe">
+    <file name="../../../../source/native/ios/NativePopup.mm" />
+</target>')
 @:cppFileCode('extern "C" void ios_show_alert(const char* title, const char* message);')
 extern class IOSNative
 {
@@ -13,3 +18,4 @@ class IOSNative
     public static function showAlert(title:String, message:String):Void {}
 }
 #end
+    
