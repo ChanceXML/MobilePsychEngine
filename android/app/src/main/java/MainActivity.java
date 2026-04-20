b2fca9cc75efb5504574150b33dc16b340f1f298
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-04-19 20:16:52 -0700

    Update MainActivity.java

--- android/app/src/main/java/MainActivity.java
+++ android/app/src/main/java/MainActivity.java
@@ -1,9 +1,15 @@
+package com.shadowmario.psychengine;
+
+import android.os.Bundle;
+import android.content.Intent;
+import org.haxe.extension.Extension;
+import org.haxe.lime.GameActivity;
+
 public class MainActivity extends GameActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         SAFBridge.activity = this;
     }
 
@@ -11,7 +17,7 @@ public class MainActivity extends GameActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == SAFModImporter.REQUEST_CODE_PICK_FOLDER) {
+        if (requestCode == SAFModImporter.REQUEST_CODE_PICK_FOLDER && resultCode == -1) {
             SAFModImporter.handleFolderResult(this, data);
         }
     }
