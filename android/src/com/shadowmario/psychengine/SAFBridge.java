1cdd0826bf79ed88e8cd1f10b3a39ea1aaa1d701
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-04-19 20:22:37 -0700

    Update SAFBridge.java

--- android/src/com/shadowmario/psychengine/SAFBridge.java
+++ android/src/com/shadowmario/psychengine/SAFBridge.java
@@ -1,25 +1,28 @@
 package com.shadowmario.psychengine;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 
 public class SAFBridge {
 
-    public static Activity activity;
-    public static String lastTreeUri = null;
+    public static MainActivity activity;
+    public static int REQUEST_CODE = 777;
 
     public static void openModsFolderPicker() {
-    if (activity == null) return;
+        if (activity == null) return;
 
-    activity.runOnUiThread(new Runnable() {
-        @Override
-        public void run() {
-            SAFModImporter.openFolderPicker(activity);
-        }
-    });
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+
+        activity.startActivityForResult(intent, REQUEST_CODE);
     }
 
-    public static void syncMods() {
-        if (activity == null || lastTreeUri == null) return;
-        SAFModSync.sync(activity, lastTreeUri);
+    public static void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == -1) {
+            Uri uri = data.getData();
+
+            AndroidModSync.setFolder(uri.toString());
+        }
     }
 }
